ea85ae386ef456bcadb8734d005672074c887e44
Olivia Gometz <oliviagometz@gmail.com>
2019-07-20 23:39:41 +0200

    feat: change iframe content with form

--- assets/js/signature-iframe.js
+++ assets/js/signature-iframe.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    console.log(cookie.read('text'));
+    var inputs = JSON.parse(cookie.read('inputs'));
+
+    inputs.forEach(function(element) {
+        for (name in element) {
+            value = element[name];
+
+            if (value == '') {
+                $('[data-remove="'+name+'"]').remove();
+                $('[data-remove2="'+name+'"]').remove();
+                $('[data-opacity="'+name+'"]').attr('color', '#ffffff');
+            }
+
+            $('[data-id="'+name+'"]').each(function() {
+                if (value == '') {
+                    $(this).remove();
+                } else {
+                    if ($(this).is('a')) {
+                        if (value.startsWith('+')) {
+                            $(this).attr('href', 'tel:' + value);
+                        } else {
+                            $(this).attr('href', value);
+                        }
+                    } else {
+                        $(this).text(value);
+                    }
+                }
+            });
+        }
+    });
 });
--- assets/js/signature.js
+++ assets/js/signature.js
@@ -1,11 +1,38 @@
-$('form').on('keyup paste change', function() {
-    var text = $('input', $(this)).val();
+$('[type="tel"]').inputmask({'mask': '+99 (9)9 99 99 99 99'});
 
-    cookie.create('text', text);
+function completeIframe() {
+    $('iframe').addClass('loading');
+
+    var inputs = [];
+
+    $('input').each(function() {
+        element = {};
+        id = $(this).attr('id');
+        val = $(this).val().trim();
+
+        if (id.startsWith('function')) {
+            val = val.toUpperCase();
+        }
+
+        element[id] = val;
+        inputs.push(element);
+    });
+
+    cookie.create('inputs', JSON.stringify(inputs));
 
     $('iframe').attr('src', function(i, val) {
         return val;
     });
+}
+
+$(document).ready(function() {
+    completeIframe();
+
+    $('iframe').on('load', function(){
+        $(this).removeClass('loading');
+    });
 });
 
-
+$('form').on('keyup paste change', function() {
+    completeIframe();
+});
