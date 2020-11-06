69a0803727a3aaad4f77b6b068c0cdfde3818820
Olivia Gometz <oliviagometz@gmail.com>
2020-11-07 00:02:52 +0100

    feat: use cookie values in signature iframe if exists

--- assets/js/signature-iframe.js
+++ assets/js/signature-iframe.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
     var inputs = JSON.parse(cookie.read('inputs'));
 
-    inputs.forEach(function(element) {
-        for (name in element) {
-            value = element[name];
+    if (inputs !== null) {
+        inputs.forEach(function(element) {
+            for (name in element) {
+                value = element[name];
 
-            if (value == '') {
-                $('[data-remove="'+name+'"]').remove();
-                $('[data-remove2="'+name+'"]').remove();
-                $('[data-opacity="'+name+'"]').attr('color', '#ffffff');
-            }
-
-            $('[data-id="'+name+'"]').each(function() {
                 if (value == '') {
-                    $(this).remove();
-                } else {
-                    if ($(this).is('a')) {
-                        if (value.startsWith('+')) {
-                            $(this).attr('href', 'tel:' + value);
-                        } else {
-                            $(this).attr('href', value);
-                        }
-                    } else {
-                        $(this).text(value);
-                    }
+                    $('[data-remove="'+name+'"]').remove();
+                    $('[data-remove2="'+name+'"]').remove();
+                    $('[data-opacity="'+name+'"]').attr('color', '#ffffff');
                 }
-            });
-        }
-    });
+
+                $('[data-id="'+name+'"]').each(function() {
+                    if (value == '') {
+                        $(this).remove();
+                    } else {
+                        if ($(this).is('a')) {
+                            if (value.startsWith('+')) {
+                                $(this).attr('href', 'tel:' + value);
+                            } else {
+                                $(this).attr('href', value);
+                            }
+                        } else {
+                            $(this).text(value);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
